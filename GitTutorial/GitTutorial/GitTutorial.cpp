72269da4de354487d392eaa0148609e4c5ac2072
109590047 <t109590047@ntut.org.tw>
2022-03-10 23:51:49 +0800

    gittutorial-8a-fail

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -25,6 +25,15 @@
 #include <ctime>
 
 bool Guess(int number) {
+	static int target = -1;
+
+	srand(time(NULL));
+	if (target == -1) {
+		target = rand() % 100 + 1;
+	}
+
+	if (number == target)
+		return true;
 	return false;
 }
 
