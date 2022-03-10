08ce89c0ba72abe8e05d6ceacb9a53bc6cef9d51
109590047 <t109590047@ntut.org.tw>
2022-03-10 23:44:49 +0800

    gittutorial-8

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -25,15 +25,6 @@
 #include <ctime>
 
 bool Guess(int number) {
-	static int target = -1;
-
-	srand(time(NULL));
-	if (target == -1) {
-		target = rand() % 100 + 1;
-	}
-
-	if (number == target)
-		return true;
 	return false;
 }
 
@@ -41,6 +32,7 @@ int main()
 {
 	int guess;
 	do {
+		std::cout << "Choose a number between 1 - 100:";
 		std::cin >> guess;
 	} while (!Guess(guess));
 	return 0;
