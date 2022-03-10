5a54b6f431a8aa181ca61462aa495d2c18c93748
109590047 <t109590047@ntut.org.tw>
2022-03-10 23:57:22 +0800

    gittutorial-8a-fail2

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -32,9 +32,15 @@ bool Guess(int number) {
 		target = rand() % 100 + 1;
 	}
 
-	if (number == target)
-		return true;
-	return false;
+	if (number > target) {
+		std::cout << "Smaller" << std::endl;
+		return false;
+	}
+	else if (number < target) {
+		std::cout << "Bigger" << std::endl;
+		return false;
+	}
+	return true;
 }
 
 int main()
